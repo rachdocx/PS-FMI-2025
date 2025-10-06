486a8f4c9cb712c431aa37a2bd6d269ea295bc2e
gabrielrachieru <rachieru.gabriel2016@gmail.com>
2025-10-06 13:34:21 +0300

    .

--- Laboratoare/Laborator1/problema1.R
+++ Laboratoare/Laborator1/problema1.R
@@ -1,13 +1,19 @@
 # sa se verifice daca un zar este masluit
 
 #intai simulez un zar corect 
+#c() - concatenare
 
-x <- sample(1:6, 50, replace = T)
+x <- sample(1:6, 100, replace = T, prob = c(0.5, 0.1,  0.1, 0.1, 0.1, 0.1))
 
-length(x[x == 1])
-length(x[x == 2])
-length(x[x == 3])
-length(x[x == 4])
-length(x[x == 5])
-length(x[x == 6])
+length(x[x == 1]) / 10^6
+length(x[x == 2]) / 10^6
+length(x[x == 3]) / 10^6
+length(x[x == 4]) / 10^6
+length(x[x == 5]) / 10^6
+length(x[x == 6]) / 10^6
 
+hist(x)
+
+#Tema:
+#construiti o functie in R care sa genereze un zar masluit pe mai multe fete indicand
+#probabilitatile fetelor favorizate iar celelalte probabilitati sa fie calculate implicit (0.1/10 bonus)
