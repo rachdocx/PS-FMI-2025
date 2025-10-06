f8c95a87c00a2aff925e3220a3549771909ce351
gabrielrachieru <rachieru.gabriel2016@gmail.com>
2025-10-06 13:33:51 +0300

    .

--- Laboratoare/Laborator1/problema1.R
+++ Laboratoare/Laborator1/problema1.R
@@ -1,2 +1,13 @@
-# sa se verifice daca un zar 
+# sa se verifice daca un zar este masluit
+
+#intai simulez un zar corect 
+
 x <- sample(1:6, 50, replace = T)
+
+length(x[x == 1])
+length(x[x == 2])
+length(x[x == 3])
+length(x[x == 4])
+length(x[x == 5])
+length(x[x == 6])
+
